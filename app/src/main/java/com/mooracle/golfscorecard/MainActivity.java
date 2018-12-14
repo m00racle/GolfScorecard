fc89889fce4453baa99820b32a23b4111e192cde
mooracle <yanuar.prakosa@gmail.com>
2018-12-14 18:31:15 +0700

    Preparing Activities and Layouts

--- app/src/main/java/com/mooracle/golfscorecard/MainActivity.java
+++ app/src/main/java/com/mooracle/golfscorecard/MainActivity.java
@@ -2,12 +2,23 @@ package com.mooracle.golfscorecard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    public Button buttonStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonStart = findViewById(R.id.buttonStart);
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
