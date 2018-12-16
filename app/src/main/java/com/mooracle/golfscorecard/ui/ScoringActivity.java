3644582657fdc75b5a37b9b7027c1b511a355b58
mooracle <yanuar.prakosa@gmail.com>
2018-12-16 12:29:54 +0700

    setting the Recycler View and adapter

--- app/src/main/java/com/mooracle/golfscorecard/ui/ScoringActivity.java
+++ app/src/main/java/com/mooracle/golfscorecard/ui/ScoringActivity.java
@@ -2,29 +2,42 @@ package com.mooracle.golfscorecard.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.Button;
 import android.widget.Toast;
 import com.mooracle.golfscorecard.R;
+import com.mooracle.golfscorecard.adapter.HoleAdapter;
 import com.mooracle.golfscorecard.model.Hole;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class ScoringActivity extends AppCompatActivity {
     private Button clearButton;
+    private RecyclerView recyclerScore;
+    private HoleAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scoring);
         clearButton = findViewById(R.id.clearButton);
+        recyclerScore = findViewById(R.id.recyclerScore);
 
         //initiate the Hole array:
         Hole[] holes = initiateHoles(18);
 
-        //test holes initiations:
-        /*String checkHole = "Hole set: ";
-        for (Hole hole : holes){
-            checkHole = checkHole + String.valueOf(hole.getHoleNumber());
-        }
-        Toast.makeText(ScoringActivity.this,checkHole, Toast.LENGTH_LONG).show();*/
+        //make holes into list:
+        List<Hole> holeList = Arrays.asList(holes);
+
+        //set adapter
+        adapter = new HoleAdapter(holeList);
+        recyclerScore.setAdapter(adapter);
+
+        //set layout manager and bind into recyclerScore
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerScore.setLayoutManager(layoutManager);
     }
 
     private Hole[] initiateHoles(int numHole) {
