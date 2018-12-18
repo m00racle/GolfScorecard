46edec9d7ac8c4c6d6f8dcb21191d11dd927f3d7
mooracle <yanuar.prakosa@gmail.com>
2018-12-18 13:14:36 +0700

    revamp and optimize reset scores mechanism

--- app/src/main/java/com/mooracle/golfscorecard/ui/ScoringActivity.java
+++ app/src/main/java/com/mooracle/golfscorecard/ui/ScoringActivity.java
@@ -1,14 +1,17 @@
 package com.mooracle.golfscorecard.ui;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.View;
-import android.widget.Button;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import com.mooracle.golfscorecard.R;
 import com.mooracle.golfscorecard.adapter.HoleAdapter;
 import com.mooracle.golfscorecard.model.Hole;
@@ -19,6 +22,7 @@ import java.util.List;
 public class ScoringActivity extends AppCompatActivity {
     private static final String PREFS_FILE = "com.mooracle.golfscorecard.preferences";
     private Hole[] holes;
+    private HoleAdapter adapter;
     // SET SHARED PREFERENCES
     private SharedPreferences sharedPreferences;
     private SharedPreferences.Editor editor;
@@ -27,12 +31,13 @@ public class ScoringActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scoring);
-        Button clearButton = findViewById(R.id.clearButton);
+
         RecyclerView recyclerScore = findViewById(R.id.recyclerScore);
         sharedPreferences = getSharedPreferences(PREFS_FILE, Context.MODE_PRIVATE);
 
         //initiating editor
         editor = sharedPreferences.edit();
+        editor.apply();
 
 
         //initiate the Hole array:
@@ -42,7 +47,8 @@ public class ScoringActivity extends AppCompatActivity {
         List<Hole> holeList = Arrays.asList(holes);
 
         //set adapter
-        final HoleAdapter adapter = new HoleAdapter(holeList);
+
+        adapter = new HoleAdapter(holeList);
         recyclerScore.setAdapter(adapter);
 
         //optimize recycler view
@@ -54,22 +60,51 @@ public class ScoringActivity extends AppCompatActivity {
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerScore.setLayoutManager(layoutManager);
 
-        // BUTTON CLEAR TO CLEAR ALL SHARED PREFERENCES AND RELOAD THE ACTIVITY.
-        clearButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                for (Hole hole : holes){
-                    hole.setScore(0);
-                }
+        // : DELETE BUTTON CLEAR TO CLEAR ALL SHARED PREFERENCES AND RELOAD THE ACTIVITY.
+    }
 
-                //clear the data inside editor to save memory:
-                editor.clear();
-                editor.apply();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //set the menu to inflate the Action bar and subtitute the clear score button!!
+        //we can delete the clear scores button after this is set!
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.score_menu, menu);
+        return true;
+    }
 
-                //switch from refresh the whole activity to just notify the adapter: <- to save memory
-                adapter.notifyDataSetChanged();
-            }
-        });
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.resetScore) {//put out an Alert that this will erase all of the scores
+            //Alert dialog is a static class it can be called directly with no initialization and addressing
+            new AlertDialog.Builder(this)
+                    .setTitle("WARNING!")
+                    .setMessage("This Will Reset All of Your Scores!, Are You Sure?")
+                    .setIcon(R.drawable.reset_icon)
+                    .setPositiveButton(R.string.alert_yes, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            //if the user press Yes reset all scores:
+                            resetScores(adapter);
+                        }
+                    })
+                    .setNegativeButton(R.string.alert_no, null) //if the user press no don't do anything
+                    .show(); //show the alert dialog
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void resetScores(HoleAdapter adapter) {
+        //this method handles the resetting of all scores:
+        for (Hole hole : holes){
+            hole.setScore(0);
+        }
+
+        //clear the data inside editor to save memory:
+        editor.clear();
+        editor.apply();
+
+        //switch from refresh the whole activity to just notify the adapter: <- to save memory
+        adapter.notifyDataSetChanged();
     }
 
     // OVERRIDE ON PAUSE METHOD AND PUT ALL INT SCORE through editor (I initialize it here)
